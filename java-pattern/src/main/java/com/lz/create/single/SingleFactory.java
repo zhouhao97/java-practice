a7a3d874ff2b99f6f7a3e35eb6a29edcab984b32
zhouhao <1907873272@qq.com>
2019-03-10 16:36:03 +0800

    first commit

---
+++ java-pattern/src/main/java/com/lz/create/single/SingleFactory.java
@@ -0,0 +1,8 @@
+package com.lz.create.single;
+
+public class SingleFactory {
+
+    public void a(){
+        System.out.println("测试");
+    }
+}
---
+++ java-pattern/src/test/java/com/lz/create/single/SingleFactoryTest.java
@@ -0,0 +1,13 @@
+package com.lz.create.single;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SingleFactoryTest {
+
+    @Test
+    public void a() {
+        new SingleFactory().a();
+    }
+}
